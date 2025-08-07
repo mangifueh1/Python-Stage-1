8901be1f0cae67b4f2ffdd947a50ffc08a1897ef
marvyk <fuehmangi410@gmail.com>
2025-08-08 00:54:49 +0100

    project complete

--- number_guessing.py
+++ number_guessing.py
@@ -1,2 +1,35 @@
-thislist = ["apple", "banana", "cherry"]
-print(thislist)
+import random
+
+print('Number Guessing Program')
+print('\nGuess my number between 1 - 100')
+
+random_guess = random.randint(0, 100)
+
+print(random_guess)
+player_input = int(input("\nGuess: "))
+result = False
+
+# attempts = 5
+
+for x in range(0, 4):
+    if player_input == random_guess:
+        print(f'You guessed Correct. My number was {random_guess}')
+        result = True
+        break
+
+    elif player_input > random_guess:
+        print(f'{4 - x} attempts left')
+        print('Go a little lower')
+        player_input = int(input("\nGuess again: "))
+        # attempts -= 1
+
+    elif player_input < random_guess:
+        print(f'{4 - x} attempts left')
+        print('Go a little higher')
+        player_input = int(input("\nGuess again: "))
+        # attempts -= 1
+
+if result:
+    print('\nYou Win')
+else:
+    print('\nYou Lose')
