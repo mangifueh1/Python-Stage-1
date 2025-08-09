92668b24058a001337babf408698283bf8090c3f
marvyk <fuehmangi410@gmail.com>
2025-08-09 11:47:51 +0100

    calculator complete

--- calculator.py
+++ calculator.py
@@ -1,1 +1,40 @@
 print('Calculator Program')
+
+print('\n" + " For Addition \n" - " For Subtraction \n" * " For Multiplication \n" / " For Division')
+
+
+def operation(operator):
+    numbers = input("\nEnter numbers then press enter: ")
+    total = int(numbers)
+
+    while True:
+        print(f'result: {total}')
+        numbers = input("\nEnter next number: ")
+        if numbers.isdigit():
+            if str(operator).strip() == "+":
+                total = total + int(numbers)
+            if str(operator).strip() == "-":
+                total = total - int(numbers)
+            if str(operator).strip() == "/":
+                try:
+                    total = total / int(numbers)
+                except ZeroDivisionError:
+                    numbers = print("You can't divide by zero. Try again: ")
+            if str(operator).strip() == "*":
+                total = total * int(numbers)
+        else:
+            print(f'\nThe answer is {total}')
+            break
+
+
+def calculator():
+    while True:
+        operator = input('\nSelect operator: '.upper()).strip()
+        if operator == "+" or operator == "-" or operator == "*" or operator == "/":
+            operation(operator)
+        else:
+            print('\nError: Please select a valid operator and try again')
+            break
+
+
+calculator()
